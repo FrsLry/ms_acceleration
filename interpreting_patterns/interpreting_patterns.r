4d220a1061d55e1d682e6a4df8d55a92ec6d1c42
petrkeil <pkeil@seznam.cz>
2024-12-02 16:11:16 +0100

    figures_done

--- interpreting_patterns/interpreting_patterns.r
+++ interpreting_patterns/interpreting_patterns.r
@@ -5,102 +5,95 @@ library(mgcv)
 library(geodata)
 library(tidyverse)
 library(randomForest)
+library(psych)
 
 rm(list = ls())
 
 ## Load the shapefile
 routes <- st_read("data/routes_shp/routes_selected_lines.shp") %>% arrange(routes)
 
-# bounding_box
-bbox <- st_bbox(routes)
-# extend the bbox by 2 degrees each direction
-bbox["xmin"] <- bbox$xmin-2
-bbox["ymin"] <- bbox$ymin-2
-bbox["xmax"] <- bbox$xmax+2
-bbox["ymax"] <- bbox$ymax+2
-
 # LAND COVER CLASS AREA --------------------------------------------------------
-# downloaded data are at a 30 sec resolution (~1 km at equator)
 
-grass <- geodata::landcover("grassland", path="interpreting_patterns/rasters")
-grass <- terra::crop(grass, bbox)
-grass <- terra::aggregate(grass, fact = 10, fun = sum)
-# writeRaster(grass, filename="interpreting_patterns/rasters/grassland.tif", overwrite=TRUE)
+grass <- terra::rast("interpreting_patterns/rasters/grassland.tif")
+routes$grass <- sqrt(terra::extract(grass, routes, fun = sum)[,2])
 
-plot(grass, col=map.pal("viridis", 100))
+shrubs <- terra::rast("interpreting_patterns/rasters/shrubs.tif")
+routes$shrubs <- sqrt(terra::extract(shrubs, routes, fun = sum)[,2])
 
-routes$grass <- terra::extract(grass, routes, fun = sum)[,2]
+trees <- terra::rast("interpreting_patterns/rasters/trees.tif")
+routes$trees <- sqrt(terra::extract(trees, routes, fun = sum)[,2])
+
+built <- terra::rast("interpreting_patterns/rasters/built.tif")
+routes$built <- sqrt(terra::extract(built, routes, fun = sum)[,2])
+
+wetland <- terra::rast("interpreting_patterns/rasters/wetland.tif")
+routes$wetland <- sqrt(terra::extract(wetland, routes, fun = sum)[,2])
+
+water <- terra::rast("interpreting_patterns/rasters/water.tif")
+routes$water <- sqrt(terra::extract(water, routes, fun = sum)[,2])
+
+# NPP (MOD17) ------------------------------------------------------------------
+NPP <- terra::rast("interpreting_patterns/rasters/NPP.tif")
+routes$NPP <- terra::extract(NPP, routes, fun = mean)[,2]
+
+# WorldClim PRECIPITATION AND TEMPERATURE --------------------------------------
+temp <- terra::rast("interpreting_patterns/rasters/temp.tif")
+routes$temp <- terra::extract(temp, routes, fun = mean)[,2]
+
+precip <- terra::rast("interpreting_patterns/rasters/precip.tif")
+routes$precip <- terra::extract(precip, routes, fun = mean)[,2]
+
+# TEMPERATURE TREND (1991-2020) ------------------------------------------------
+temp91to20 <- terra::rast("interpreting_patterns/rasters/T_trend.tif")
+routes$temp91to20 <- terra::extract(temp91to20, routes, fun = mean)[,2]
+
+# N FERTILIZER USAGE -----------------------------------------------------------
+Nfertilizer <- terra::rast("interpreting_patterns/rasters/Nfertilizer.tif")
+routes$Nfertilizer <- terra::extract(Nfertilizer, routes, fun = mean)[,2]
 
 
 # CROPLAND DENSITY AND ITS TEMPORAL CHANGE (2003-2019) -------------------------
-# downloaded data are at a 30 sec resolution (~1 km at equator)
+crops03 <- terra::rast("interpreting_patterns/rasters/crops03.tif")
+crops19 <- terra::rast("interpreting_patterns/rasters/crops19.tif")
 
-crops03 <- geodata::cropland(source="GLAD", year=2003, path="interpreting_patterns/rasters")
-crops03 <- terra::crop(crops03, bbox)
-crops03 <- terra::aggregate(crops03, fact = 10, fun = sum)
-
-crops19 <- geodata::cropland(source="GLAD", year=2019, path="interpreting_patterns/rasters")
-crops19 <- terra::crop(crops19, bbox)
-crops19 <- terra::aggregate(crops19, fact = 10, fun = sum)
-
-delta.crops <- crops19 - crops03
-
-plot(crops03, col=map.pal("viridis", 100))
-plot(delta.crops, col=map.pal("differences", 100))
-
-# extract the values from the rasters to routes
 routes$crops03 <- terra::extract(crops03, routes, fun = sum)[,2]
 routes$crops19 <- terra::extract(crops19, routes, fun = sum)[,2]
-routes$delta.crops <- routes$crops19 - routes$crops03
+routes$crops03to19 <- routes$crops19 - routes$crops03
 
-
-
+# NDVI CHANGE (1982-2012) ------------------------------------------------------
+NDVI82to12 <- terra::rast("interpreting_patterns/rasters/NDVI82to12.tif")
+routes$NDVI82to12 <- terra::extract(NDVI82to12, routes, fun = mean)[,2]
 
 # HUMAN FOOTPRINT AND ITS TEMPORAL CHANGE (1993-2009) --------------------------
-# downloaded data are at a 30 sec resolution (~1 km at equator)
-
-footprint93 <- geodata::footprint(year = 1993, path = "interpreting_patterns/rasters")
-footprint93 <- terra::crop(footprint93, bbox)
-footprint93 <- terra::aggregate(footprint93, fact = 10, fun = mean)
-
-footprint09 <- geodata::footprint(year = 2009, path = "interpreting_patterns/rasters")
-footprint09 <- terra::crop(footprint09, bbox)
-footprint09 <- terra::aggregate(footprint09, fact = 10, fun = mean)
-
+footprint93 <- terra::rast("interpreting_patterns/rasters/footprint93.tif")
+footprint09 <- terra::rast("interpreting_patterns/rasters/footprint09.tif")
 delta.footprint <- footprint09 - footprint93
 
 plot(footprint93, col=map.pal("viridis", 100))
 plot(delta.footprint, col=map.pal("differences", 100))
 lines(routes)
 
-hist(delta.footprint)
-
 # extract the values from the rasters to routes
 routes$footprint93 <- terra::extract(footprint93, routes, fun = mean)[,2]
 routes$footprint09 <- terra::extract(footprint09, routes, fun = mean)[,2]
-routes$delta.footprint <- terra::extract(delta.footprint, routes, fun = mean)[,2]
-
-
+routes$footprint93to09 <- terra::extract(delta.footprint, routes, fun = mean)[,2]
 
 
 # POPULATION DENSITY, GPW dataset ----------------------------------------------
 
 # load the 2.5 min raster with human population density
-pop2000 <-rast("interpreting_patterns/rasters/gpw_v4_population_count_rev11_2000_2pt5_min.tif")
-pop2000 <- terra::crop(pop2000, bbox)
+pop2000 <-rast("interpreting_patterns/rasters/pop2000.tif")
 
 # extract the population density values, summing the values of 2.5 min pixels touching a route
-routes$pop2000 <- terra::extract(pop2000, routes, fun = sum)[,2]
+routes$log10pop2000 <- log10(terra::extract(pop2000, routes, fun = sum)[,2] + 1)
 
 
+# ------------------------------------------------------------------------------
 ## Load the models' output
 for(file in list.files("mixed_model/outputs/", full.names = T)){
   assign(gsub("^mixed_model/outputs/|\\.rds$","",file),
          readRDS(file = file))
 }
-
-## Add values of beta1 (slope) to the shapefile
-routes$growth_rate <- overall_g_output$mean$beta1
 
 
 # ------------------------------------------------------------------------------
@@ -110,11 +103,41 @@ coords <- data.frame(st_centroid(routes) %>% st_coordinates() )
 names(coords) <- c("lon","lat")
 routes <- cbind(routes, coords)
 
+## Add raw (not smoothed) values delta_g to the dataset
+routes$delta_g <- overall_g_output$mean$beta1
+
 # fit the gam model
-growth_rate_gam <- gam(growth_rate ~ s(lon, lat, bs = "gp", k = 100, m = 2),
+growth_rate_gam <- gam(delta_g ~ s(lon, lat, bs = "gp", k = 100, m = 2),
                        data = routes)
-routes$delta_g <- growth_rate_gam$fitted.values
+routes$delta_g_gam <- growth_rate_gam$fitted.values
+
 # ------------------------------------------------------------------------------
+
+# preparing the data for correlation matrix plot
+data.frame(names(routes))
+dat <- as.data.frame(routes[, 24:ncol(routes)])
+dat <- select(dat, -c("geometry")) # remove the last "geometry" column
+
+# ------------------------------------------------------------------------------
+
+# correlation matrix plot from the "psych" package
+
+png("interpreting_patterns/all_pairs.png", width= 2500, height = 2500)
+
+psych::pairs.panels(dat,
+                    ellipses = FALSE,
+                    density = TRUE,
+                    smooth = TRUE,
+                    ci = TRUE,
+                    stars = TRUE,
+                    scale = TRUE)
+
+dev.off()
+
+
+
+
+
 
 plot(footprint93 ~ log10(crops03), data = routes)
 plot(footprint93 ~ log10(pop2000), data = routes)
