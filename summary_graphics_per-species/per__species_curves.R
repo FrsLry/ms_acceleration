603e0f99a0b1b410e4df690ce8825c8e212feb40
petrkeil <pkeil@seznam.cz>
2025-03-18 13:25:13 +0100

    means changed to medians, plus uncertainty added

--- summary_graphics_per-species/per__species_curves.R
+++ summary_graphics_per-species/per__species_curves.R
@@ -39,6 +39,32 @@ trend.plotter <- function(mat, poly.deg = 3, family = "gaussian", reg.col=reg.co
 
 
 # ------------------------------------------------------------------------------
+# function that takes a matrix of means and matrix of SDs, aggregates time series
+# across sites, and plots the aggregated (summed) values together with their
+# Bayesian unceratinty
+aggregate.uncertainty <- function(mean.mat, sd.mat, Nrep = 500)
+{
+  N <- nrow(mean.mat)*ncol(mean.mat) # N of all cells in the matrix
+  smp.sums <- matrix(ncol = ncol(mean.mat),
+                     nrow = Nrep) # empty matrix for sample storage
+
+  for(i in 1:Nrep)
+  {
+    smp.mat <- matrix(rnorm(N, mean.mat, sd.mat), # take a draw from the normal
+                      nrow = nrow(mean.mat),
+                      ncol = ncol(mean.mat),
+                      byrow = FALSE)
+    #smp.mat[smp.mat < 0] <- 0 # replace negative values with 0
+    smp.sums[i,] <- colSums(smp.mat, na.rm = TRUE) # aggregate across sites
+  }
+  res <- apply(X=smp.sums, MARGIN=2, FUN=quantile, probs = c(0.025, 0.975))
+
+  return(res)
+}
+# x <- aggregate.uncertainty(Smat, Smat.sd, Nrep=100)
+
+
+# ------------------------------------------------------------------------------
 
 
 # main loop through all species
@@ -51,6 +77,7 @@ for(filename in filelist)
   # clean species name for nice file output
   species <- gsub(".rds", replacement = "", x = filename)
   species <- gsub("summary_", replacement = "", x = species)
+  print(species)
 
   # extract matrix of C from the data
   spec <- dat.all[species][[species]]
@@ -62,21 +89,27 @@ for(filename in filelist)
   R <- summ[grepl("R", row.names(summ)),]
 
   # extract global parameters
-  gamma <- summ[grepl("gamma", row.names(summ)),]$mean
+  gamma50 <- summ[grepl("gamma", row.names(summ)),]$"50%"
   gamma25 <- summ[grepl("gamma", row.names(summ)),]$"2.5%"
   gamma975 <- summ[grepl("gamma", row.names(summ)),]$"97.5%"
-  phi <- summ[grepl("phi", row.names(summ)),]$mean
+  phi50 <- summ[grepl("phi", row.names(summ)),]$"50%"
   phi25 <- summ[grepl("phi", row.names(summ)),]$"2.5%"
   phi975 <- summ[grepl("phi", row.names(summ)),]$"97.5%"
 
   # estimated survival S (number of surviving individuals)
-  Smat <- matrix(S$mean, ncol = 34, nrow = 1033, byrow=FALSE)
+  Smat.mean <- matrix(S$mean, ncol = 34, nrow = 1033, byrow=FALSE)
+  Smat <- matrix(S$"50%", ncol = 34, nrow = 1033, byrow=FALSE)
+  Smat.sd <- matrix(S$mean, ncol = 34, nrow = 1033, byrow=FALSE)
 
   # recruitment R (number of recruited individuals)
-  Rmat <- matrix(R$mean, ncol = 34, nrow = 1033, byrow=FALSE)
+  Rmat.mean <- matrix(R$mean, ncol = 34, nrow = 1033, byrow=FALSE)
+  Rmat <- matrix(R$"50%", ncol = 34, nrow = 1033, byrow=FALSE)
+  Rmat.sd <- matrix(R$sd, ncol = 34, nrow = 1033, byrow=FALSE)
 
   # abundance N
-  Nmat <- matrix(N$mean, ncol = 35, nrow = 1033, byrow=FALSE)
+  Nmat.mean <- matrix(N$mean, ncol = 35, nrow = 1033, byrow=FALSE)
+  Nmat <- matrix(N$"50%", ncol = 35, nrow = 1033, byrow=FALSE)
+  Nmat.sd <- matrix(N$sd, ncol = 35, nrow = 1033, byrow=FALSE)
 
   # check if they the conversion to matrix went well
   Smat[1:5, 1:5]
@@ -88,15 +121,15 @@ for(filename in filelist)
   min.ind<-which( rowMeans(Nmat) %in% min(rowMeans(Nmat)) )
 
   # summary table of the main  parameters
-  sm <- summ[c(1:20),c("mean", "sd", "2.5%", "97.5%", "Rhat")]
+  sm <- summ[c(1:20),c("mean", "sd", "2.5%", "50%", "97.5%", "Rhat")]
   sm <- data.frame(parameter = rownames(sm),
                    mean = round(sm$mean,2),
                    SD = round(sm$sd,2),
                    q2.5 = round(sm$"2.5%",2),
+                   q50 = round(sm$"50%", 2),
                    q97.5 = round(sm$"97.5%",2),
                    Rhat = sm$Rhat)
   converged <- max(sm$Rhat, na.rm=T) < 1.1 # has the model converged
-
 
   # parameters of the graphical output
   max.col <- "#4daf4a" # marking time series with max mean abundance
@@ -122,7 +155,7 @@ for(filename in filelist)
     # mark species that haven't converged
     if(converged == FALSE)
     {
-      text(x=0.07, y=0.5, "NOT\nCONVERGED", col="red", cex = 3)
+      text(x=0.06, y=0.5, "NOT\nCONVERGED", col="red", cex = 3)
     }
 
     # reset figure margins
@@ -170,7 +203,7 @@ for(filename in filelist)
             main = "Survival rate")
     lines(SdivNmat[max.ind,], col = max.col, lwd=2)
     lines(SdivNmat[min.ind,], col = min.col, lwd=2)
-    abline(h = phi, lwd= 2, col=par.col)
+    abline(h = phi50, lwd= 2, col=par.col)
     abline(h = phi25, lwd= 1, col=par.col, lty=2)
     abline(h = phi975, lwd= 1, col=par.col, lty=2)
     trend.plotter(SdivNmat, reg.col=reg.col)
@@ -187,7 +220,7 @@ for(filename in filelist)
 
     # artificial recruitment as a random draw from poission
     RmatArti <- Rmat
-    RmatArti[] <- rpois(n=ncol(Rmat)*nrow(Rmat), lambda=gamma)
+    RmatArti[] <- rpois(n=ncol(Rmat)*nrow(Rmat), lambda=gamma50)
     matplot(t(RmatArti), col=gray(0.3, alpha = alpha),
             type = "l",lty = 1, ylab = "Rt+1", xlab="t+1",
             ylim=c(0, max(Rmat)),
@@ -200,7 +233,7 @@ for(filename in filelist)
             main = "Recruitment")
     lines(Rmat[max.ind,], col = max.col, lwd=2)
     lines(Rmat[min.ind,], col = min.col, lwd=2)
-    abline(h = gamma, lwd= 2, col=par.col)
+    abline(h = gamma50, lwd= 2, col=par.col)
     abline(h = gamma25, lwd= 1, col=par.col, lty=2)
     abline(h = gamma975, lwd= 1, col=par.col, lty=2)
     trend.plotter(Rmat, reg.col=reg.col)
@@ -231,9 +264,22 @@ for(filename in filelist)
     plot(1:35, Ntot, type = "l", lty = 1, lwd = 2, ylim = c(0, max(Ntot)),
          ylab = "Number of individuals", xlab="t",
          main = "Total sums of count variables")
-    lines(1:35, Ctot, lwd = 2, lty = 1, col = "grey")
-    lines(2:35, Stot, lwd = 2, lty = 3)
-    lines(2:35, Rtot, lwd = 2, lty = 4)
+      Nunc <- aggregate.uncertainty(Nmat, Nmat.sd, Nrep = 500)
+      lines(1:35, Nunc[1,], lwd = 1, lty = 1)
+      lines(1:35, Nunc[2,], lwd = 1, lty = 1)
+
+      lines(1:35, Ctot, lwd = 2, lty = 1, col = "grey")
+      # total survival
+      lines(2:35, Stot, lwd = 2, lty = 3)
+      Sunc <- aggregate.uncertainty(Smat, Smat.sd, Nrep = 500)
+      lines(2:35, Sunc[1,], lwd = 1, lty = 3)
+      lines(2:35, Sunc[2,], lwd = 1, lty = 3)
+      # total recruitment
+      lines(2:35, Rtot, lwd = 2, lty = 4)
+      Runc <- aggregate.uncertainty(Rmat, Rmat.sd, Nrep = 500)
+      lines(2:35, Runc[1,], lwd = 1, lty = 4)
+      lines(2:35, Runc[2,], lwd = 1, lty = 4)
+
     legend("topright", legend = c("Nt", "Ct", "St", "Rt"),
            lty = c(1,1,3,4),
            lwd = c(2,2,2,2),
